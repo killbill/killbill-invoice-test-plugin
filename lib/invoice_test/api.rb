8045aa3b607870c9b95e650a482013beda3bc875
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-02-02 03:32:47 -0800

    api: add support for repairs and adjustments
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/invoice_test/api.rb
+++ lib/invoice_test/api.rb
@@ -5,10 +5,60 @@ module InvoiceTest
     def get_additional_invoice_items(invoice, properties, context)
       additional_items = []
       invoice.invoice_items.each do |original_item|
-        additional_items << build_item(original_item, original_item.amount * 7 / 100, 'Tax item', :TAX) unless original_item.amount == 0
+        if original_item.amount > 0 and [:EXTERNAL_CHARGE, :FIXED, :RECURRING, :USAGE].include?(original_item.invoice_item_type)
+          current_tax_amount  = total_taxed_amount_for_item(invoice, original_item).round(2)
+          expected_tax_amount = compute_tax_amount(invoice, original_item).round(2)
+          if current_tax_amount < expected_tax_amount
+            # Add missing TAX item
+            additional_items << build_item(original_item, expected_tax_amount - current_tax_amount, 'Tax item', :TAX)
+          elsif current_tax_amount > expected_tax_amount
+            # Item adjust the TAX item
+            adjustment_amount  = (current_tax_amount - expected_tax_amount).round(2)
+            tax_item_to_adjust = find_tax_item_to_adjust(invoice, original_item, adjustment_amount)
+            additional_items << build_item(tax_item_to_adjust, -adjustment_amount, 'Tax item', :ITEM_ADJ)
+          end
+        end
       end
 
       additional_items
+    end
+
+    private
+
+    def compute_tax_amount(original_invoice, original_item)
+      net_amount(original_invoice, original_item) * 7.0 / 100
+    end
+
+    def find_tax_item_to_adjust(original_invoice, original_item, amount_to_adjust)
+      original_invoice.invoice_items.find do |ii|
+        ii.linked_item_id == original_item.id and ii.invoice_item_type == :TAX and net_amount(original_invoice, ii) >= amount_to_adjust
+      end
+    end
+
+    # Positive or null amount
+    def total_taxed_amount_for_item(original_invoice, original_item)
+      original_invoice.invoice_items.inject(0) do |sum, ii|
+        if ii.linked_item_id == original_item.id and ii.invoice_item_type == :TAX
+          sum + net_amount(original_invoice, ii)
+        else
+          sum
+        end
+      end
+    end
+
+    def net_amount(original_invoice, original_item)
+      original_item.amount + total_adjusted_amount_for_item(original_invoice, original_item)
+    end
+
+    # Negative amount
+    def total_adjusted_amount_for_item(original_invoice, original_item)
+      original_invoice.invoice_items.inject(0) do |sum, ii|
+        if ii.linked_item_id == original_item.id and [:ITEM_ADJ, :REPAIR_ADJ].include?(ii.invoice_item_type)
+          sum + ii.amount
+        else
+          sum
+        end
+      end
     end
   end
 end
--- spec/invoice_test/base_plugin_spec.rb
+++ spec/invoice_test/base_plugin_spec.rb
@@ -18,22 +18,89 @@ describe InvoiceTest::InvoicePlugin do
     @plugin.stop_plugin
   end
 
-  it 'should add additional item' do
-    item        = Killbill::Plugin::Model::InvoiceItem.new
-    item.id     = SecureRandom.uuid
-    item.amount = 100
+  it 'should add two additional items' do
+    fixed_item     = build_invoice_item(100, :FIXED)
+    recurring_item = build_invoice_item(150, :RECURRING)
+    invoice        = build_invoice([fixed_item, recurring_item])
 
-    invoice               = Killbill::Plugin::Model::Invoice.new
-    invoice.id            = SecureRandom.uuid
-    invoice.invoice_items = [item]
+    new_items = @plugin.get_additional_invoice_items(invoice, @properties, @call_context)
+    new_items.size.should == 2
+
+    check_invoice_item(new_items[0], 7, fixed_item)
+    check_invoice_item(new_items[1], 10.5, recurring_item)
+
+    check_idempotency(invoice, new_items)
+  end
+
+  it 'should add no additional item' do
+    fixed_item         = build_invoice_item(100, :FIXED)
+    recurring_item     = build_invoice_item(150, :RECURRING)
+    recurring_adj_item = build_invoice_item(-150, :ITEM_ADJ, recurring_item.id)
+    fixed_adj_item     = build_invoice_item(-100, :ITEM_ADJ, fixed_item.id)
+    invoice            = build_invoice([fixed_item, recurring_item, recurring_adj_item, fixed_adj_item])
+
+    new_items = @plugin.get_additional_invoice_items(invoice, @properties, @call_context)
+    new_items.size.should == 0
+
+    check_idempotency(invoice, new_items)
+  end
+
+  it 'should add a single additional item' do
+    fixed_item     = build_invoice_item(100, :FIXED)
+    recurring_item = build_invoice_item(150, :RECURRING)
+    fixed_adj_item = build_invoice_item(-100, :ITEM_ADJ, fixed_item.id)
+    invoice        = build_invoice([fixed_item, recurring_item, fixed_adj_item])
 
     new_items = @plugin.get_additional_invoice_items(invoice, @properties, @call_context)
     new_items.size.should == 1
 
-    new_item = new_items.first
-    new_item.should be_an_instance_of Killbill::Plugin::Model::InvoiceItem
-    new_item.amount.should == 7
-    new_item.invoice_item_type.should == :TAX
-    new_item.linked_item_id.should == item.id
+    check_invoice_item(new_items[0], 10.5, recurring_item)
+
+    check_idempotency(invoice, new_items)
+  end
+
+  it 'should handle partial adjustments' do
+    fixed_item     = build_invoice_item(100, :FIXED)
+    tax_item       = build_invoice_item(7, :TAX, fixed_item.id)
+    fixed_adj_item = build_invoice_item(-20, :ITEM_ADJ, fixed_item.id)
+    invoice        = build_invoice([fixed_item, tax_item, fixed_adj_item])
+
+    new_items = @plugin.get_additional_invoice_items(invoice, @properties, @call_context)
+    new_items.size.should == 1
+
+    check_invoice_item(new_items[0], -1.4, tax_item, :ITEM_ADJ)
+
+    check_idempotency(invoice, new_items)
+  end
+
+  private
+
+  def build_invoice(items)
+    invoice               = Killbill::Plugin::Model::Invoice.new
+    invoice.id            = SecureRandom.uuid
+    invoice.invoice_items = items
+    invoice
+  end
+
+  def build_invoice_item(amount, invoice_item_type=:FIXED, linked_item_id=nil)
+    item                   = Killbill::Plugin::Model::InvoiceItem.new
+    item.id                = SecureRandom.uuid
+    item.amount            = amount
+    item.invoice_item_type = invoice_item_type
+    item.linked_item_id    = linked_item_id
+    item
+  end
+
+  def check_invoice_item(item, amount, original_item, invoice_item_type=:TAX)
+    item.should be_an_instance_of Killbill::Plugin::Model::InvoiceItem
+    item.amount.should == amount
+    item.invoice_item_type.should == invoice_item_type
+    item.linked_item_id.should == original_item.id
+  end
+
+  def check_idempotency(original_invoice, new_items)
+    invoice          = build_invoice(original_invoice.invoice_items + new_items)
+    additional_items = @plugin.get_additional_invoice_items(invoice, @properties, @call_context)
+    additional_items.size.should == 0
   end
 end
